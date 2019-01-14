12a9a0757508432d547f75725f23d87ae9aa51c8
Leonardo Brito <lbrito@gmail.com>
2019-01-14 17:42:13 -0300

    Adds db seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,6 @@ foo1 = Foo.create
 foo2 = Foo.create
 
 foo1.image.attach(io: File.open('./db/seeds/mars.jpg'), filename: 'mars.jpg', content_type: 'application/jpg')
-foo1.image.attach(io: File.open('./db/seeds/mars.jpg'), filename: 'mars.jpg', content_type: 'application/jpg')
+
+# This hangs
+foo2.image.attach(io: File.open('./db/seeds/mars.jpg'), filename: 'mars.jpg', content_type: 'application/jpg')
